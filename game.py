39e7f89106b681cf6db97b14ffd497d5af6af9eb
Kaikaine <kairijb@protonmail.com>
2020-04-27 12:51:47 -0500

    add function to change player location

--- game.py
+++ game.py
@@ -18,9 +18,20 @@ class Player:
         print(self.items)
         return
 
-    def move(self, direction):
-        pass
-
+    def move_room(self):
+        command = input("Please choose from one of these commands. 'N' 'S' 'E' 'W': ")
+        if command.lower() == "n":
+            Room(self.location).area.move_north
+        elif command.lower() == "e":
+            self.location = self.location.move_east()
+        elif command.lower == "s":
+            Room.area.move_south
+        elif command.lower() == "w":
+            Room.area.move_west
+        elif command.lower == "q":
+            pass
+        else:
+            print("I did not recognize that command \n")
 
 
 class Room:
@@ -37,17 +48,28 @@ class Outside:
         self.east = east
         self.west = west
 
-    def move_north(self, north):
-        pass
+    def move_north(self):
+        Player.location = self.north
+        return
 
-    def move_east(self, north):
-        pass
+    def move_east(self):
+        print(f"Player moved to: {self.east} \n")
+        return self.east
 
-    def move_south(self, north):
-        pass
+    def move_south(self):
+        Player.location = self.south
+        return
 
-    def move_west(self, north):
-        pass
+    def move_west(self):
+        Player.location = self.west
+        return
+
+    def __str__(self):
+        return self.name
 
 
 print(f"Hello {player_name} and welcome to my game")
+new_player = Player(["flashlight"], Outside("Outside", "Tower entrance", None, "car", None, ["key"]), player_name)
+print(new_player.location)
+new_player.move_room()
+new_player.current_location()
